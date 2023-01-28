9f80628f52bb0933caa7f24d2a292c09736620ae
Sonit2006 <117414032+Sonit2006@users.noreply.github.com>
2023-01-28 13:44:35 -0600

    wrist subsystem

--- src/main/java/frc/robot/subsystems/WristSubsystem.java
+++ src/main/java/frc/robot/subsystems/WristSubsystem.java
@@ -1,10 +1,15 @@
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class WristSubsystem extends SubsystemBase{
-    CANSparkMax neo = new CANSparkMax(17, MotorType.kBrushless);
-
-    
-}
+    CANSparkMax motor = new CANSparkMax(17, MotorType.kBrushless);
+    public void positive () {
+        motor.set(1);
+     }    
+     public void negative () {
+        motor.set(-1);
+     }    
+    }

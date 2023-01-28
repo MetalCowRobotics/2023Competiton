e64bddbeb9d2685634cd43aa2ae809d1f9685f55
Jake <jaketc05@gmail.com>
2023-01-28 11:32:54 -0600

    Update ShoulderSubsystem.java

--- src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -1,6 +1,53 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+
 public class ShoulderSubsystem extends SubsystemBase{
+    AnalogPotentiometer pot = new AnalogPotentiometer(0, 180, 30);
+    TalonSRX cim15 = new TalonSRX(15);
+
+    public void startMotor(){
+        cim15.set(TalonSRXControlMode.PercentOutput, .3);
+    }
+
+    public void startMotorReverse(){
+        cim15.set(TalonSRXControlMode.PercentOutput, -.3);        
+    }
     
+    public void stopMotor(){
+        cim15.set(TalonSRXControlMode.PercentOutput, 0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+

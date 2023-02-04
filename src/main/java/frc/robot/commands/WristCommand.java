692de38ffb551e4175bbc4a17fb52acc1441584a
Ehimmell <himmellethan@gmail.com>
2023-02-04 14:48:03 -0600

    Fixed errors with main

--- src/main/java/frc/robot/commands/WristCommand.java
+++ src/main/java/frc/robot/commands/WristCommand.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class WristSubsystem extends SubsystemBase{
+public class WristCommand extends SubsystemBase{
     CANSparkMax motor = new CANSparkMax(17, MotorType.kBrushless);
     public void positive () {
         motor.set(1);

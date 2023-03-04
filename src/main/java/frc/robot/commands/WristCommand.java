a9bf222e0b18ec1738c0cefbea14467055be6c0c
Sonit2006 <117414032+Sonit2006@users.noreply.github.com>
2023-03-03 19:33:54 -0600

    Create WristCommand.java

---
+++ src/main/java/frc/robot/commands/WristCommand.java
@@ -0,0 +1,7 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class WristCommand extends CommandBase{
+    
+}

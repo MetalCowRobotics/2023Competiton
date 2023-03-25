99f09e428f7f582a39fe772039cbafe4d31dc167
AndyGuzKip <97487151+AndyGuzKip@users.noreply.github.com>
2023-03-25 12:49:58 -0500

    Update AutoBalance.java

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -1,7 +1,5 @@
 package frc.robot.commands;
 
-import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -13,19 +11,14 @@ import frc.robot.subsystems.Swerve;
 public class AutoBalance extends CommandBase{
     
     private Swerve m_Swerve;
-    private BooleanSupplier robotCentricSup;
 
-    private PIDController rollPIDController = new PIDController(0.6, 0, 0);
     private PIDController drivePIDController = new PIDController(.8,0,0);
 
     private double target = 0;
 
-    public AutoBalance(Swerve m_Swerve, BooleanSupplier robotCentricSup) {
+    public AutoBalance(Swerve m_Swerve) {
         this.m_Swerve = m_Swerve;
         addRequirements(m_Swerve);
-        this.robotCentricSup = robotCentricSup;
-
-        rollPIDController.setSetpoint(0);
 
         drivePIDController.setSetpoint(target);
     }
@@ -43,24 +36,22 @@ public class AutoBalance extends CommandBase{
         }
 
         if (roll > 180) {
-            rollPIDController.setSetpoint(360);
+            drivePIDController.setSetpoint(x + 0.02);
         } else {
-            rollPIDController.setSetpoint(0);
+            drivePIDController.setSetpoint(x - 0.02);
         }
-
-        double rotation = rollPIDController.calculate(roll);
+        
         double Correction = drivePIDController.calculate(x);
 
         SmartDashboard.putNumber("absolute roll", roll);
         if (Math.abs(roll - 180) < 2) {
-            rotation = 0;
             Correction = 0;
         }
         
         m_Swerve.drive(
             new Translation2d(Correction, 0).times(Constants.Swerve.maxSpeed), 
-            -rotation * Constants.Swerve.maxAngularVelocity, 
-            !robotCentricSup.getAsBoolean(), 
+            0, 
+            true, 
             false
         );
     }
